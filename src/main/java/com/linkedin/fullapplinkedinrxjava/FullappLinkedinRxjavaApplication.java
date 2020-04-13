96889f563c5306da84e5b4a549c1cdc4e5a345d2
Sandeep Dass <sandeepdass003@gmail.com>
2020-04-13 16:58:38 +0530

    Initial checkin

---
+++ src/main/java/com/linkedin/fullapplinkedinrxjava/FullappLinkedinRxjavaApplication.java
@@ -0,0 +1,13 @@
+package com.linkedin.fullapplinkedinrxjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullappLinkedinRxjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullappLinkedinRxjavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/linkedin/fullapplinkedinrxjava/FullappLinkedinRxjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linkedin.fullapplinkedinrxjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullappLinkedinRxjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
